1ad561cc0c44b83f7bb2bb85b4430be388b0369e
kirill <kirimihi@rambler.ru>
2018-07-23 03:31:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kirill/myapplication/LoveActivity.java
@@ -0,0 +1,37 @@
+package com.example.kirill.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class LoveActivity extends AppCompatActivity {
+
+    private static final String USER_TEXT = "flagResId";
+    private TextView textView;
+
+    public static void show(Context context, String text) {
+        final Intent intent = new Intent(context, LoveActivity.class);
+        intent.putExtra(LoveActivity.USER_TEXT, text);
+        context.startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_love);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String stuff = bundle.getString("stuff");
+        textView = (TextView) findViewById(R.id.text);
+        textView.setText(stuff);
+
+    }
+}
---
+++ app/src/main/java/com/example/kirill/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.kirill.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button applyButton;
+    private Button emailButton;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailButton = (Button) findViewById(R.id.button_email);
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) findViewById(R.id.plain_text_input);
+                final String text = textView.getText().toString();
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"testkirimihiniki@email.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "The message I wrote on my app");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, text);
+
+                startActivity(Intent.createChooser(emailIntent, "Send Email"));
+            }
+        });
+
+    }
+
+    public void sendMessage(View view) {
+        Intent i = new Intent(MainActivity.this, LoveActivity.class);
+        TextView textView = (TextView) findViewById(R.id.plain_text_input);
+        String text = textView.getText().toString();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("stuff", text);
+
+        i.putExtras(bundle);
+
+        startActivity(i);
+    }
+}
