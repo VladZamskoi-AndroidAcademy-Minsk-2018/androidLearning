d420666f8a1b0f2b00d8febcabd3935385f59587
kirill <kirimihi@rambler.ru>
2018-07-29 20:44:05 +0300

    new task

--- app/src/main/java/com/example/kirill/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kirill/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kirill.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,47 +11,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button applyButton;
-    private EditText editText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button emailButton = findViewById(R.id.button_email);
-        Button applyButton = findViewById(R.id.button_activity);
+        Button browserButton = findViewById(R.id.browser);
 
-        emailButton.setOnClickListener(new View.OnClickListener() {
+        browserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView textView = findViewById(R.id.plain_text_input);
-                final String text = textView.getText().toString();
-                Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                emailIntent.setType("plain/text");
-                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"testkirimihiniki@email.com"});
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "The message I wrote on my app");
-                emailIntent.putExtra(Intent.EXTRA_TEXT, text);
-
-                startActivity(Intent.createChooser(emailIntent, "Send Email"));
+                Uri webpage = Uri.parse("https://github.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
 
-        applyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            Intent i = new Intent(MainActivity.this, LoveActivity.class);
-            TextView textView = findViewById(R.id.plain_text_input);
-            String text = textView.getText().toString();
-
-            Bundle bundle = new Bundle();
-            bundle.putString("stuff", text);
-
-            i.putExtras(bundle);
-
-            startActivity(i);
-            }
-        });
         }
 
     }
